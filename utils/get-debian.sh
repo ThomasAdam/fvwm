c8e4cff6527b313bf2cf6e0d8845453f10a903e6
Thomas Adam <thomas@fvwm.org>
2011-08-24 19:12:36 +0100

    Minor tweaks to get-debian.sh
    * Don't bother creating a subshell to run git commands in.
    * Check for existence of debian/ first; bail if exists.

--- utils/get-debian.sh
+++ utils/get-debian.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+[ -d debian ] && exit 1
+
 mkdir debian && {
-(
     cd debian
-    git init
+    git init && \
     git remote add -t fvwm -m fvwm -f origin \
-        git://git.debian.org/~srivasta/debian/debian-dir.git
-    git pull
-    git submodule init
+        git://git.debian.org/~srivasta/debian/debian-dir.git && \
+    git pull && \
+    git submodule init && \
     git submodule update
-)
 } || exit 1
-    
+
 [ -f utils/changelog-debian-v-261.patch ] &&
     git apply utils/changelog-debian-v-261.patch
